1246f54ff915b91f17055cd39eef0410d9b7a433
Andrei Pranko <djmentarey@gmail.com>
2019-01-19 17:42:32 +0300

    configured DI

--- app/src/main/java/com/android/mentarey/mytestapplication/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/android/mentarey/mytestapplication/di/component/ApplicationComponent.kt
@@ -3,7 +3,6 @@ package com.android.mentarey.mytestapplication.di.component
 import com.android.mentarey.mytestapplication.presentation.MainActivity
 import com.android.mentarey.mytestapplication.di.modules.AndroidModule
 import com.android.mentarey.mytestapplication.di.modules.DependenciesModule
-import com.android.mentarey.mytestapplication.di.modules.RepositoryModule
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 import javax.inject.Singleton
@@ -11,10 +10,11 @@ import javax.inject.Singleton
 @Singleton
 @Component(
     modules =
-    [AndroidInjectionModule::class,
+    [
+        AndroidInjectionModule::class,
         AndroidModule::class,
-        RepositoryModule::class,
-        DependenciesModule::class]
+        DependenciesModule::class
+    ]
 )
 interface ApplicationComponent {
     fun inject(mainActivity: MainActivity)
--- app/src/main/java/com/android/mentarey/mytestapplication/di/modules/DependenciesModule.kt
+++ app/src/main/java/com/android/mentarey/mytestapplication/di/modules/DependenciesModule.kt
@@ -1,15 +1,18 @@
 package com.android.mentarey.mytestapplication.di.modules
 
 import android.content.Context
+import com.android.mentarey.mytestapplication.data.repository.ProductRepository
+import com.android.mentarey.mytestapplication.data.repository.ProductRepositoryImpl
 import com.android.mentarey.mytestapplication.presentation.models.MyObjectBox
 import com.android.mentarey.mytestapplication.presentation.models.Product
+import com.android.mentarey.mytestapplication.presentation.mvp.MainPresenter
 import dagger.Module
 import dagger.Provides
 import io.objectbox.Box
 import io.objectbox.BoxStore
 import javax.inject.Singleton
 
-@Module
+@Module(includes = [AndroidModule::class])
 class DependenciesModule {
     @Singleton
     @Provides
@@ -21,5 +24,18 @@ class DependenciesModule {
     @Provides
     fun provideProductBox(boxStore: BoxStore): Box<Product> {
         return boxStore.boxFor(Product::class.java)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideProductRepository(productBox: Box<Product>): ProductRepository {
+        return ProductRepositoryImpl(productBox)
+    }
+
+    @Singleton
+    @Provides
+    fun provideMainPresenter(productRepository: ProductRepository): MainPresenter {
+        return MainPresenter(productRepository)
     }
 }
